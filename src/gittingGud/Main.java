0c9aaa41bbc0c3ec4937e440618dc27b34ebc6f9
Michał Filo <michal.filo6@gmail.com>
2017-04-05 16:36:13 +0200

    Getting started
    * Creating online repository
    * With simple java code

---
+++ src/gittingGud/Main.java
@@ -0,0 +1,8 @@
+package gittingGud;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Work start = new Work();
+    }
+}
---
+++ src/gittingGud/Work.java
@@ -0,0 +1,10 @@
+package gittingGud;
+
+/**
+ * Created by HunReh on 2017-04-05.
+ */
+public class Work {
+    public Work(){
+
+    }
+}
