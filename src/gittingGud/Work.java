d6743627c30b98d1fe8f31dbaa0477b371c291d5
Michał Filo <michal.filo6@gmail.com>
2017-04-06 14:51:23 +0200

    Added comment for deletion (reset/reverse)

--- src/gittingGud/Work.java
+++ src/gittingGud/Work.java
@@ -8,6 +8,7 @@ package gittingGud;
 // Another comment and try to update repository from InteliJ
 // One more comment2!
 // Multiple branches comment
+// Comment for dletion
 
 public class Work {
     public Work(){
